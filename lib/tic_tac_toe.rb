8a7cba610837a16c1acbaca478feb1de00a5c376
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2020-01-07 06:13:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,7 +38,7 @@ class TicTacToe
   end
   
   def valid_move?(position)
-    position.between?(0,8) && position_taken == FALSE
+    position.between?(0,8) && position_taken == "false"
   end
   
   
